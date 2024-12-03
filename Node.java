195ba0013b74057345e7d1e238be09d2d78e0da6
HunterMcCallister <huntermccallister@gmail.com>
2024-12-03 12:35:24 -0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,43 @@
+
+public class Node<T> {
+	private T element;
+	private Node<T> nextNode;
+	private Node<T> previousNode;
+	
+	/** initialize a new node with given element */
+	public Node(T element) {
+		this.element = element;
+		nextNode = null;
+		previousNode = null;
+	}
+	
+	/** Initialized a new node with given element and next node. */
+	public Node(T element, Node<T> nextNode) {
+		this.element = element;
+		this.nextNode = nextNode;
+	}
+
+	public T getElement() {
+		return element;
+	}
+	public void setElement(T element){
+		this.element = element;
+	}
+
+	public Node<T> getNextNode() {
+		return nextNode;
+	}
+
+	public void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	public Node<T> getPreviousNode() {
+		return previousNode;
+	}
+
+	public void setPreviousNode(Node<T> previousNode) {
+		this.previousNode = previousNode;
+	}
+
+}
